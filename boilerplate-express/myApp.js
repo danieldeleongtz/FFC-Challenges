aa385d8203b49a8d7def3e493db2c7895ecd2035
danieldeleongtz <danieldeleongt97@gmail.com>
2021-09-27 21:40:14 -0500

    Task 14 Add Static files to public path

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -3,15 +3,16 @@ var app = express();
 
 
 console.log('Hello World');
+absolutePath = __dirname + '/public';
+app.use('/public', express.static(absolutePath));
 
 
-app.get('/', (req, res) => {
-    absolutePath = __dirname + '/views/index.html';
-    console.log(absolutePath);
+app.get('/public', (req, res) => {
+    absolutePath2 = __dirname + '/views/index.html';
+    console.log(absolutePath2);
     //res.send('Hello Express');
-    res.sendFile(absolutePath);
+    res.sendFile(absolutePath2);
 });
-
 
 
 
